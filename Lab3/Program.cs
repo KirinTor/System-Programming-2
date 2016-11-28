9ab215d64d49212806a53eefbff20fb68899b254
KirinTor <kirintor830@gmail.com>
2016-11-28 22:58:59 +0200

    Simple Lexical Analyzer implemented

---
+++ Lab3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    static class LexicalAnalyzer
+    {
+        private static string[] terminals = new string[]
+        {"==", "++", "--", "=", "?", ":", "*", "[", "]", ";", "{", "}", "+", "-", "/", "(", ")", "<", ">"};
+        public static void LexicalAnalyze(this string analyzedString)
+        {
+            string[] variables = analyzedString.Split(terminals, StringSplitOptions.None);
+            foreach (string str in variables)
+            {
+                if (!str.Equals(""))
+                {
+                    try
+                    {
+                        int value = Int32.Parse(str);
+                        Console.WriteLine(value + " is a number");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(str + " is a variable");
+                    }
+                }
+            }
+            foreach (string token in terminals)
+            {
+                if (analyzedString.Contains(token))
+                {
+                    analyzedString = analyzedString.Replace(token, "");
+                    Console.WriteLine(token + " is a token");
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "b=c?d:2*a*[m];";
+            Console.WriteLine("Input string:  " + str);
+            str.LexicalAnalyze();
+
+            Console.ReadKey();
+        }
+    }
+}
